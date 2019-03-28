eb7b955784133acf7a98a6b549d414cf7555b592
Alex <ordgie@gmail.com>
2019-03-28 19:41:28 +0300

    Ok

---
+++ TASK/script.js
@@ -0,0 +1,17 @@
+/*let age = document.getElementById('age')
+   ;
+
+        function showUser(surname, name) {
+	        alert("Пользователь " + surname + " " + name + ", его возраст " + this.age);
+        }
+        showUser();*/
+
+'use strict'
+
+function hello() {
+
+console.log(this);
+
+}
+
+hello();
